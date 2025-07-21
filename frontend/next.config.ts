eaa518c18f32bae4bd3034c81abbbe8cf10acce3
nikola43 <pauloxti@gmail.com>
2025-07-21 19:39:12 +0200

    fix build cloudflare

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
   compress: true,
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+  webpack: (config, { isServer }) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    // config.optimization.splitChunks = {
+    //   chunks: 'all',
+    //   maxSize: 244 * 1024,
+    //   minSize: 20 * 1024
+    // }
     config.optimization.splitChunks = {
       chunks: 'all',
-      maxSize: 244 * 1024,
-      minSize: 20 * 1024
+      maxSize: 200 * 1024, // Reduced from 244KB
+      minSize: 20 * 1024,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 25,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          maxSize: 200 * 1024,
+        },
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          maxSize: 200 * 1024,
+        }
+      }
+    };
+
+    if (!isServer && process.env.NODE_ENV === 'production') {
+      config.cache = false;
     }
     return config
   }
