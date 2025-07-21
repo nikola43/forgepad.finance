548e14ab469bcae62dcc5ca590d6ea7190285382
truth4you <forever617830@gmail.com>
2025-07-22 02:16:26 +0900

    update config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // output: 'export',
+  compress: true,
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 244 * 1024,
+      minSize: 20 * 1024
+    }
     return config
   }
 };
