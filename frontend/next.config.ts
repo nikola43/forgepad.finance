1bef6735fe9b57c573684f7e65fba16517071d56
nikola43 <pauloxti@gmail.com>
2025-07-23 18:31:41 +0200

    fix build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,28 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   // output: 'export',
+//   compress: true,
+//   webpack: config => {
+//     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+//     config.optimization.splitChunks = {
+//       chunks: 'all',
+//       maxSize: 244 * 1024,
+//       minSize: 20 * 1024,
+//     }
+//     config.cache = false
+//     return config
+//   }
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  compress: true,
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      maxSize: 244 * 1024,
-      minSize: 20 * 1024,
-    }
-    config.cache = false
     return config
   }
 };
