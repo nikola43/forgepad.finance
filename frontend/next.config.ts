c0ad2e5f581e2f8b4d1e16f09572b392fb92764b
nikola43 <pauloxti@gmail.com>
2025-07-23 18:26:40 +0200

    build fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export',
   compress: true,
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
