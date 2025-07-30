31371bafdf3361f79de6920c339558505affc9f1
nikola43 <pauloxti@gmail.com>
2025-07-30 18:20:11 +0200

    added pancake logo

--- backend/app/config/web3.config.js
+++ backend/app/config/web3.config.js
@@ -53,7 +53,7 @@ const CHAINS = [
     virtualTokenAmount: 800000000,
     totalSupply: 1000000000,
     targetMarketCap: 69000,
-    pools: ['uniswap:v2', 'uniswap:v3']
+    pools: ['pancakeswap:v2', 'pancakeswap:v3']
   },
   {
     name: 'Solana',
