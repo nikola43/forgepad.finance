57a2adf330ad337843e06896098e423bcffa6f98
nikola43 <pauloxti@gmail.com>
2025-11-15 01:27:10 +0100

    fix build

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,4 +1,4 @@
-export const isDevEnv = true // process.env.REACT_APP_ENV==="development"
+export const isDevEnv = false // process.env.REACT_APP_ENV==="development"
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "d12f34d58e09877ad612f47b1f0b8001" // this is a public projectId only to use on localhost
@@ -16,3 +16,4 @@ export const FILE_ENDPOINT = isDevEnv ? `${API_ENDPOINT}/uploads` : `https://fil
 export const FORGE_TELEGRAM_URL = "https://t.me/forgepad"
 export const FORGE_TWITTER_URL = "https://x.com/forgepad"
 export const FORGE_WEBSITE_URL = "https://forgepad.finance"
+
