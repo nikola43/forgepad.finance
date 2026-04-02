ef432f615c926c36c8b833295d865843f71edb38
paulo <pauloxti@gmail.com>
2026-04-02 05:05:13 +0200

    tests

--- backend/app/config/web3.config.js
+++ backend/app/config/web3.config.js
@@ -49,7 +49,7 @@ const CHAINS = [
     // rpcUrl: 'https://bsc-rpc.publicnode.com',
     // rpcUrl: 'https://rpc.ankr.com/bsc/21abecc585cbd8eb9f1fb38ee79dff11d3b3d8dafbca2589236ea7dcc4d88593',
     explorerUrl: 'https://bscscan.com',
-    contractAddress: '0x17F38AB89496E10E34268bDc66ae48D50Fb4A227',
+    contractAddress: '0x6B24c6bdDf7ce47723B329374b58b5c2BA942d83',
     abi: abiEthism.v1,
     virtualEthAmount: 6,
     virtualTokenAmount: 900000000,
